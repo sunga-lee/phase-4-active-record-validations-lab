58f19566399f6bd04bb0a060eeb5b2688dc649aa
sunga-lee <sungajunior1@gmail.com>
2023-06-24 12:34:18 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction, No Fiction)}
+    validates :title_is_clickbait
+    def title_is_clickbait
+        if !(["Won't Believe","Secret","Top [number]","Guess"].any?{|word|"#{title}".include?(word)})
+            errors.add(:title, "is not clickbait")
+        end
+    end
 end
